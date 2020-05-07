ce8103f3d8cdfe3dc6bfaabba695672a4fc50fec
Sebastien Morel <sebastien.morel@davidson.fr>
2020-05-07 18:24:07 +0200

    create template driven form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './features/header/header.component';
 import { PlayerModule } from './features/player/player.module';
+import { LoginModule } from './features/login/login/login.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { PlayerModule } from './features/player/player.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PlayerModule
+    PlayerModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent],
