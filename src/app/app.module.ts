9f396295b9336cb751fd2622b4749cac3c279bb7
Sebastien Morel <sebastien.morel@davidson.fr>
2020-05-07 18:42:11 +0200

    delete extra directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './features/header/header.component';
 import { PlayerModule } from './features/player/player.module';
-import { LoginModule } from './features/login/login/login.module';
+import { LoginModule } from './features/login/login.module';
 
 @NgModule({
   declarations: [
